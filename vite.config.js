ff88daf874f3bd9cf6fe2a6cf9eb9344c1a87c69
Devendan <solankidevendan@gmail.com>
2026-02-26 23:44:00 +0530

    Initial Stage

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+   preview: {
+    host: true,
+    port: process.env.PORT || 4173,
+    allowedHosts: [
+      'expensefrontend-production.up.railway.app'
+    ]
+  }
+
 })
